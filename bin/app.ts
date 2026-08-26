31c295cd397c3b0ef4b4b6201294faa16741fb44
hyoaru <the.jadecabrera@gmail.com>
2026-08-26 15:51:11 +0800

    feat: database stack

--- bin/app.ts
+++ bin/app.ts
@@ -2,9 +2,18 @@
 import * as cdk from "aws-cdk-lib/core";
 import "dotenv/config";
 import { IdentityStack } from "../lib/identity";
+import { DatabaseStack } from "../lib/database";
 
 const app = new cdk.App();
+
 new IdentityStack(app, "ItalaStagingIdentityStack", {
+  env: {
+    account: process.env.STAGING_ACCOUNT_ID,
+    region: process.env.AWS_REGION,
+  },
+});
+
+new DatabaseStack(app, "ItalaStagingDatabaseStack", {
   env: {
     account: process.env.STAGING_ACCOUNT_ID,
     region: process.env.AWS_REGION,
---
+++ lib/database.ts
@@ -0,0 +1,44 @@
+import { aws_dynamodb as dynamodb } from "aws-cdk-lib";
+import * as cdk from "aws-cdk-lib/core";
+import { Construct } from "constructs";
+
+export class DatabaseStack extends cdk.Stack {
+  public table: dynamodb.TableV2;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.table = new dynamodb.TableV2(this, "Table", {
+      tableName: "itala",
+      tableClass: dynamodb.TableClass.STANDARD,
+      partitionKey: { name: "PK", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "SK", type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      deletionProtection: false,
+      billing: dynamodb.Billing.onDemand(),
+      encryption: dynamodb.TableEncryptionV2.awsManagedKey(),
+      globalSecondaryIndexes: [
+        {
+          indexName: "TransactionByOccurredAt",
+          partitionKey: { name: "GSI1PK", type: dynamodb.AttributeType.STRING },
+          sortKey: { name: "GSI1SK", type: dynamodb.AttributeType.STRING },
+        },
+        {
+          indexName: "TransactionByType",
+          partitionKey: { name: "GSI2PK", type: dynamodb.AttributeType.STRING },
+          sortKey: { name: "GSI2SK", type: dynamodb.AttributeType.STRING },
+        },
+        {
+          indexName: "TransactionByAccount",
+          partitionKey: { name: "GSI3PK", type: dynamodb.AttributeType.STRING },
+          sortKey: { name: "GSI3SK", type: dynamodb.AttributeType.STRING },
+        },
+        {
+          indexName: "TransactionByCategory",
+          partitionKey: { name: "GSI4PK", type: dynamodb.AttributeType.STRING },
+          sortKey: { name: "GSI4SK", type: dynamodb.AttributeType.STRING },
+        },
+      ],
+    });
+  }
+}
